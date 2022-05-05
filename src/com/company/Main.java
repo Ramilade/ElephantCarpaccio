756a8a602c2cb42c618061d6d0b74307de2dc844
KristianWede <kris79s4@stud.kea.dk>
2022-05-05 10:48:17 +0200

    Added printRabat

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,29 +14,39 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("""
-                
+                                
                 Velkommen til prisberegner 2022!! v 0.42.
-                
+                                
                 Indtast venligst et nummer for at tage et valg.
-                
+                                
                 1. Beregn pris af produkter.
                 2. Udskriv alle rabatter.
                 3. Udksriv alle skatter.
-                
+                                
                 """);
 
         String choice = sc.nextLine();
 
-        switch(choice){
-           // case "1" -> beregn();
-           // case "2" -> printRabat();
-           // case "3" -> printTax();
-           // default -> System.out.println("Venligst indtast et tal som er validt.");
+        switch (choice) {
+            // case "1" -> beregn();
+            case "2" -> printRabat();
+            // case "3" -> printTax();
+            // default -> System.out.println("Venligst indtast et tal som er validt.");
+        }
+    }
+
+        public void printRabat(){
+            System.out.println("""
+                    Ordre værdi	    Rabat mængde
+                    $1,000	        3%
+                    $5,000	        5%
+                    $7,000	        7%
+                    $10,000	        10%
+                    $50,000	        15%
+                    """);
         }
 
 
-
-    }
 
     public static void main(String[] args) {
 	Main main = new Main();
